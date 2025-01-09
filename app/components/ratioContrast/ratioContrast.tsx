bb84ba0bef8e1afd929eff6c5755f85e43a45475
Varun <vvarunsharma64@gmail.com>
2025-01-09 13:03:34 +0530

    will update soon

--- app/components/ratioContrast/ratioContrast.tsx
+++ app/components/ratioContrast/ratioContrast.tsx
@@ -40,6 +40,7 @@ export function RatioContrast({
   };
 
   return (
+    // final colours may look different
     <div className="flex flex-col gap-2">
       <span className="text-sm font-medium">Contrast</span>
 
