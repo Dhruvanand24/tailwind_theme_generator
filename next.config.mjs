14d0fc1e948fe7e9e49d296b2533bab30f94d1ed
dhruvanand24 <ananddhruv475@gmail.com>
2025-01-10 11:29:26 +0530

    Fixed Build Error
    Co-authored-by: VarunWeb6 <varunweb6@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, //required for no build errors
   },
   images: {
     remotePatterns: [
